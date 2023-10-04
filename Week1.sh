1696b6084c85cce05bb215f138e60ca6e56f49f0
Esmond Burke <eburkecenter@gmail.com>
2023-10-04 17:43:25 -0500

    Initial Commint for all Different Scripts

---
+++ Week1.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+# Welcome to Week 1 of Google Automation Final Project written in Bash 
---
+++ week1.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python3
+"""Welcome to Week 1 of Google Automation Final Project written in Python """
