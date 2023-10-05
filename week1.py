2b750f4a77e86af336999fcafb6b6f4eea6a9c6f
Esmond Burke <eburkecenter@gmail.com>
2023-10-04 21:47:29 -0500

    Update Python code to add functionality. Update Java to include main to get rid of errors

--- week1.py
+++ week1.py
@@ -1,2 +1,39 @@
 #!/usr/bin/env python3
-"""Welcome to Week 1 of Google Automation Final Project written in Python """
+""" Welcome to Week 1 of Google Automation Final Project written in Python 
+
+                            Manipulating Images 
+"""
+#importing library
+from PIL import Image
+import os
+from multiprocessing import Pool
+# Reading images from location
+#images = input("Please enter path to location of images: ")
+
+#Hard path for testing purposes
+#path = "C:/Users/eburk/Desktop/Screenshots2"
+input_path = r"C:\Users\eburk\Desktop\pics"
+output_path =r"C:\Users\eburk\Desktop\pics2"
+angle = 90
+size = (128,128)
+#reading images
+image_folder = os.listdir(input_path)
+
+# Creating a Pool for multi threading.
+pool = Pool(4)
+
+""" 
+     Creating a function that does the following: 
+# 1. Rotate the image 90° clockwise
+# 2. Resize the image from 192x192 to 128x128
+# 3. Save the image to a new folder in .jpeg format
+
+"""
+def process_image(image,folder,angle,size,output):
+    im = Image.open(folder+r"\\"+ image)
+    new_image = im.rotate(angle).resize(size)
+    new_path = os.path.join(output,image)
+    new_image.save(new_path,"jpeg")
+
+for image in image_folder:
+    process_image(image,input_path,angle,size,output_path)
