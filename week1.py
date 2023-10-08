44823464b17f6ed7374cb5b2ba293deda8956826
Esmond Burke <eburkecenter@gmail.com>
2023-10-08 13:26:17 -0500

    Removed unused functions and imports

--- week1.py
+++ week1.py
@@ -3,47 +3,60 @@
 
                             Manipulating Images 
 """
+
 # importing library
 from PIL import Image
 import os
-from multiprocessing import Pool
-# Reading images from location
-# images = input("Please enter path to location of images: ")
+import sys
 
-# Hard path for testing purposes
-# path = "C:/Users/eburk/Desktop/Screenshots2"
-input_path = "C:/Users/eburk/Desktop/pics"
-output_path = "C:/Users/eburk/Desktop/pics2"
-angle = 90
+# Reading images from location
+
+# print(len(sys.argv))
+if len(sys.argv) <= 2:
+    print("Please enter arguments input path and output path")
+    sys.exit(1)
+
+input_path = sys.argv[1]
+output_path = sys.argv[2]
+# print(output_path)
+angle = 270
 size = (128, 128)
 # reading images
 image_folder = os.listdir(input_path)
 
-# Creating a Pool for multi threading.
-
-""" 
-     Creating a function that does the following: 
-# 1. Rotate the image 90° clockwise
-# 2. Resize the image from 192x192 to 128x128
-# 3. Save the image to a new folder in .jpeg format
-
-"""
-
 
 def process_image(image, folder, angle, size, output):
-    im = Image.open(folder+"/" + image)
-    new_image = im.rotate(angle).resize(size)
-    new_path = os.path.join(output, image)
-    new_image.save(new_path, "jpeg")
+    """ 
+     Creating a function that does the following: 
+    # 1. converts images to standard RBG
+    # 2. Rotate the image 90° clockwise
+    # 3. Resize the image from 192x192 to 128x128
+    # 4. Save the image to a new folder in .jpeg format
+
+    """
+    img_path = os.path.join(folder, image)
+    im = Image.open(img_path)
+    new_image = im.convert("RGB").rotate(angle).resize(size)
+    # print(new_image)
+    new_image.save(output+"/"+image, "JPEG")
+
+
+def main():
+    """Main function that is used to iterate through the image folder
+       The function skips over hidden files then calls process_image function
+       The function also catch errors that may occur when tring to process a function"""
+    for image in image_folder:
+        if image.startswith("."):
+            continue
+        else:
+            # image=image.split(".")
+            try:
+                process_image(image, input_path, angle, size, output_path)
+            except OSError as error:
+                print(error)
+                pass
 
 
 # Running program
 if __name__ == "__main__":
-    for image in image_folder:
-        if image.endswith('.ini'):
-            continue
-        try:
-            process_image(image, input_path, angle, size, output_path)
-        except OSError as error:
-            print(error)
-            pass
+    main()
