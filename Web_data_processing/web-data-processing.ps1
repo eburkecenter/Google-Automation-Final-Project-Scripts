cb81ef13c2c74d0ab5193fac1a0044d2196b1774
Esmond Burke <eburkecenter@gmail.com>
2023-10-09 21:52:57 -0500

    Added 3 other starter scripts for the other lang.

---
+++ Web_data_processing/web-data-processing.ps1
@@ -0,0 +1,2 @@
+#!/usr/bin/env pwsh
+<# Welcome to Week 2 of Google Automation Final Project written in Powershell #>
---
+++ Web_data_processing/web_data_processing.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+# Welcome to Week 2 of Google Automation Final Project written in Bash
